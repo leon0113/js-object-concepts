2dd8bf2a4348bfe08371ae28fc14019752ab0b14
leon0113 <tahjib.leon399@gmail.com>
2022-10-14 01:47:23 +0600

    object method

---
+++ method.js
@@ -0,0 +1,24 @@
+const student = {
+    name: 'Tahjib',
+    balance: 5000,
+    id: 101,
+    major: 'Math',
+    isRich: false,
+    subjects: ['English', 'Economics', 'math'],
+    bestFriend: {
+        name: 'leon',
+        major: 'Math'
+    },
+    takeExam: function () {
+        console.log(this.bestFriend.name, 'taking exam');
+    },
+    treat: function (expense, tip) {
+        this.balance = this.balance - expense - tip;
+        return this.balance;
+    }
+}
+
+student.takeExam();
+const remaining1 = student.treat(200, 50);
+const remaining2 = student.treat(250, 60);
+console.log(remaining2);
